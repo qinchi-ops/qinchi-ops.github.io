fdc17b9f6c5f695c957fc0fb9b68a1c3dbb6e821
Qinchi <raify.zhao@gmail.com>
2024-03-06 22:05:18 +0800

    fork
    Signed-off-by: Qinchi <raify.zhao@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const thisYear = new Date().getFullYear()
+const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressBarOfThisYear = generateProgressBar()
+
+function generateProgressBar() {
+    const progressBarCapacity = 30
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const progressBar =
+      '█'.repeat(passedProgressBarIndex) +
+      '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
+    return `{ ${progressBar} }`
+}
+
+const readme = `\
 ### Hi there 👋
 
-⏳ Year progress { █████▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ } 17.90 %
+⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ---
 
-⏰ Updated on Wed, 06 Mar 2024 12:14:00 GMT
+⏰ Updated on ${new Date().toUTCString()}
 
 ---
 ### My GitHub Contributions    
@@ -28,4 +44,7 @@ I'm angola, a new-media editor.
 ![Visual Studio Code](https://img.shields.io/badge/Visual_Studio_Code-007ACC?style=flat-square&logo=Visual-Studio-Code&logoColor=white)       
 ![MWeb](https://img.shields.io/badge/%E5%9B%BE%E5%BA%8A-MWeb-lightgrey)          
 ![Git](https://img.shields.io/badge/-Git-black?style=plastic&logo=git)     
+\
+`
 
+console.log(readme)
